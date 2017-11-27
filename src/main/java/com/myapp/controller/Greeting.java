51aad0abd396ed98232b64a9e5e5a323cf11e1f4
hardikge <hardikge@cybage.com>
2017-11-27 20:17:14 +0530

    initial commit

---
+++ src/main/java/com/myapp/controller/Greeting.java
@@ -0,0 +1,26 @@
+package com.myapp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.myapp.domain.Greeting;
+
+
+@Controller
+@RequestMapping("/health")
+public class Greeting {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public
+    @ResponseBody
+    Greeting process(@RequestParam(value = "content", required = false, defaultValue = "content") String content) {
+        return new Greeting(counter.incrementAndGet(), content );
+    }
+
+}
